074bdfc5e6f0a53ec3e80ca62f12674749efc6d7
robertxiaoxiao <276408734@qq.com>
2019-05-10 11:24:50 +0800

    init demo

---
+++ src/StringPatternMatch/KMP.java
@@ -0,0 +1,22 @@
+package StringPatternMatch;
+
+public class KMP {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println(kmp("abc","a"));
+
+
+    }
+
+
+    public static int kmp(String pattern ,String matcher){
+
+
+        return -1;
+    }
+
+
+
+}
