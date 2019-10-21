9d42e1b813226f727874961dabba8d7ecd777f86
robertxiaoxiao <276408734@qq.com>
2019-10-21 23:04:42 +0800

    Q4

--- src/LeetCode/Array/q4.java
+++ src/LeetCode/Array/q4.java
@@ -9,29 +9,115 @@ import java.util.concurrent.AbstractExecutorService;
 
 public class q4 {
     public static void main(String[] args) {
-        AbstractExecutorService a  ;
-
+        int[] a = {1, 3, 4};
+        int[] b = {2};
+        System.out.println(findMedianSortedArrays(b, a));
     }
 
-    public  static double findMedianSortedArrays(int[] arrayA,int[] arrayB ){
-            int m=arrayA.length;
-            int n=arrayB.length;
+    //  EDGE PROCESS IS EASIER WHEN CONSIDERING IT CLEARLY
+    public static double findMedianSortedArrays(int[] a, int[] b) {
+        int m = a.length;
+        int n = b.length;
         // arrayA[m-1] <arrayB[0] ;
 
-        if((m+n)%2==0)
-            return findInArrays(arrayA,arrayB,0,m-1,0,n-1,(m+n)/2);
-        else
-            return (findInArrays(arrayA,arrayB,0,m-1,0,n-1,(m+n)/2)+findInArrays(arrayA,arrayB,0,m-1,0,n-1,(m+n)/2+1))/2;
-    }
+        int imin = 0;
+        int imax = m;
+        //put the median in the left
+        int length = (m + n + 1) / 2;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = length - i;
+            // notice the judge condition
+            if (i < imax && b[j - 1] > a[i])
+                imin = i + 1;
+            else if (i > imin && a[i - 1] > b[j])
+                imax = i - 1;
 
-    //  array a
-    public static double findInArrays(int[] a,int[] b, int starta ,int enda,int startb,int endb ,int posatEnd)
-    {
-        int halfa=(starta+enda)/2;
-        int halfb=(startb+endb)/2;
+            else {
+                int maxleft = 0;
 
+                if (i == 0)
+                    maxleft = b[j - 1];
+                else if (j == 0)
+                    maxleft = a[i - 1];
+                else {
+                    maxleft = Math.max(b[j - 1], a[i - 1]);
+                }
+
+                if ((m + n) % 2 == 1)
+                    return maxleft;
+
+                int minright = 0;
+                if (i == m) {
+                    minright = b[j];
+                } else if (j == n) {
+                    minright = a[i];
+                } else {
+                    minright = Math.min(b[j], a[i]);
+                }
+
+                if ((m + n) % 2 == 0)
+                    return (double) (maxleft + minright) / 2;
+
+            }
+
+        }
 
         return 0;
     }
 
+    // the simply expreesion is the correct  ;
+    public double findMedianSortedArraysErroe(int[] a, int[] b) {
+        int m = a.length;
+        int n = b.length;
+        // arrayA[m-1] <arrayB[0] ;
+
+        int start = 0;
+        int end = m - 1;
+        //put the median in the left
+        int length = (m + n + 1) / 2;
+
+        int i = 0;
+        int j = length - i;
+
+        int leftmax = 0;
+        int rightmin = 0;
+
+        while (start < end) {
+            i = (start + end) / 2;
+
+            j = length - i - 2;
+            if (j == -1) {
+                leftmax = a[i];
+                rightmin = Math.min(a[i + 1], b[0]);
+                break;
+            }
+
+
+            if (i + 1 < m && j + 1 < n && a[i] <= b[j + 1] && b[j] <= a[i + 1]) {
+                System.out.println("current (i,j): " + i + "   " + j);
+                leftmax = Math.max(a[i], b[j]);
+                rightmin = Math.min(b[j + 1], a[i + 1]);
+                break;
+            }
+
+            // the end of smaller array
+            else if (j == n - 1) {
+                leftmax = Math.min(a[i + 1], b[j]);
+                rightmin = Math.max(a[i + 1], b[j]);
+                break;
+            } else if (i < m && j + 1 < n && a[i] > b[j + 1])
+                end = i;
+            else if (i + 1 < m && j < n && b[j] > a[i + 1])
+                start = i;
+
+            System.out.println("current (i,j): " + i + "    " + j);
+        }
+
+        if ((m + n) % 2 == 0)
+            return (double) (leftmax + rightmin) / 2;
+        else
+            return (double) leftmax;
+
+    }
 }
